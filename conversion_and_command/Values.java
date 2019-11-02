011ab863bfd0685a16a65aea37f0121e61a05f9e
TrikalisChristos <43909927+ChristosTrikalis@users.noreply.github.com>
2019-11-02 14:04:07 +0200

    Update Values.java

--- conversion_and_command/Values.java
+++ conversion_and_command/Values.java
@@ -2,14 +2,14 @@ package conversion_and_command;
 
 class Values
 {
-    static final int capacityOfDocuments = 18316;
+    static final int capacityOfDocuments = 1;//number of documents. First get your collection of docs
     static final String JsonFormat = ".json";
     static final String saveLocation = "C:\\path\\To\\files\\json_results\\"; //path where json docs will be saved
     static final String kibanaCommands = "C:\\path\\to\\files\\KibanaCommands\\"; //path where POST command will be saved
     static final String documentsLocation = "C:\\path\\to\\files\\Parsed files";    //path where parsed files are located
-    static final String projectName = "index_trikalis_christos";
+    static final String projectName = "index_yourElasticIndex";
     static final String projectType = "_doc";
-    static final String Info = "Information Retrieval Systems\nProject - 1st Phase \nTrikalis Christos - 3140205\n\n";
+    static final String Info = "";
     static final String prefix = "{\"index\":{\"_id\":\"";
     static final String suffix = "\"}}";
 
